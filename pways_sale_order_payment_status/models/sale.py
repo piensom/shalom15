ee57d2b325deba6a89eced58168022c389f90737
Andrea de la Rosa <71832462+Andreadlrg@users.noreply.github.com>
2022-11-15 15:37:15 -0600

    .

--- pways_sale_order_payment_status/models/sale.py
+++ pways_sale_order_payment_status/models/sale.py
@@ -13,16 +13,31 @@ class SaleOrder(models.Model):
     @api.depends('state', 'invoice_ids')
     def _compute_invoice_state(self):
         for order in self:
+            facturas = []
+            total = 0
             if order.invoice_ids:
-                if all([invoice_id.payment_state == "paid" for invoice_id in order.invoice_ids]):
+                facturas = order.invoice_ids
+            if len(facturas)>0:
+                if all([invoice_id.payment_state == "paid" for invoice_id in facturas]):
                     order.payment_status = "fully_paid"
                     order.amount_due = 0.0
-                elif all([invoice_id.payment_state == "not_paid" for invoice_id in order.invoice_ids]):
+                elif all([invoice_id.payment_state == "not_paid" for invoice_id in facturas]):
                     order.payment_status = "not_paid"
-                    order.amount_due = sum(order.invoice_ids.mapped('amount_residual'))            
+                    #order.amount_due = sum(facturas.mapped('amount_residual'))   
+                    for f in facturas:
+                        if f.state == 'posted':
+                            total += f.amount_residual
+                    order.amount_due = total        
                 else:
                     order.payment_status = "partial_paid"
-                    order.amount_due = sum(order.invoice_ids.mapped('amount_residual'))
+                    #order.amount_due = sum(facturas.mapped('amount_residual')
+                    
+                    for f in facturas:
+                        if f.state == 'posted':
+                            total += f.amount_residual
+                    #order.amount_due = len(facturas)
+                    order.amount_due = total
+                    
             else:
                 order.payment_status = "no_invoice"
                 order.amount_due = 0.0
